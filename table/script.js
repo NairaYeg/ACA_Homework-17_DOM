1b7e36098744b85a36fcefa0d45b1bf1e76a88d1
Naira Yeghiazaryan <yeghiazaryannaira97@gmail.com>
2020-07-29 19:35:33 +0400

    added icons

--- table/script.js
+++ table/script.js
@@ -13,7 +13,8 @@ const grade = document.querySelector('#grade')
 const addBtn = document.querySelector('#add-btn')
 const ageColumn = document.querySelector('#ageCol')
 const tbody = document.querySelector('#tbody')
-const sortIcon = ageCol.querySelector(".sort-icon");
+const sortIcon = document.querySelector("#sort-icon");
+
 
 const render = (students) =>{
     let [first, ...rest] = students // slice first element of students , students[0] === {order: sortingOrders.random}
@@ -46,16 +47,20 @@ addBtn.addEventListener('click', ()=>{
     render(students)
 })
 
+
 ageCol.addEventListener('click', (event) =>{
     switch (students[0].order) {
         case sortingOrders.random:
             students[0].order = sortingOrders.asc;
+            sortIcon.textContent = '△';
           break;
         case sortingOrders.asc:
             students[0].order = sortingOrders.desc;
+            sortIcon.textContent = '▽';
           break;
         case sortingOrders.desc:
             students[0].order = sortingOrders.random;
+            sortIcon.textContent = '▷';
           break;
         default:
           throw new Error(`${students[0].order} is not an valid order!`);
